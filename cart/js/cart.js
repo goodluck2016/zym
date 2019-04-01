54f1eff3587993ba0da4d6109c3df4164c21bd30
goodluck2016 <zhangyumei_2019@163.com>
2019-04-01 16:15:35 +0800

    cart

--- cart/js/cart.js
+++ cart/js/cart.js
@@ -1,16 +1,77 @@
-$(function(){
-  sum();
-  checkAll();
-  checkItem();
-  plusFn();
-  minusFn();
-  delFn();
-  delAll();
-  nogoods();
-
-  //总金额
-  function sum(){
-    var sum = 0,
+var cartObj = {
+  init: function() {
+    var _this = this;
+    _this.bindEvent();
+  },
+  bindEvent:function() {
+    var _this = this;
+    //全选
+    $('.checkAll').on('click',function(){
+      $('.data-table').find('input:checkbox').prop('checked',  $(this).prop('checked'));
+      _this.isCheckAll();
+      _this.sum();
+    });
+    //单个选中
+    $('.checkboxItem').on('click',function(){
+      _this.isCheckAll();
+      _this.sum();
+    });
+    //加
+    $('.plus').on('click',function(){
+      var num = parseInt($(this).prev('.number').val());
+      var itemPrice = parseFloat($(this).parent('.bd-number').parent('.good-item').find('.itemPrice').find('b').html());
+      num++;
+      $(this).prev('.number').val(num++);
+      _this.sum();
+    });
+    //减
+    $('.minus').on('click',function(){
+      var num = parseInt($(this).next('.number').val());
+      var itemPrice = parseFloat($(this).parent('.bd-number').parent('.good-item').find('.itemPrice').find('b').html());
+      if(num > 1){
+        num--;
+        $(this).next('.number').val(num);
+      } else if(num===1) {
+        $(this).next('.number').val(1);
+        _this.tipsBox('亲，数量不能再少啦');
+      } else {
+        $(this).next('.number').val(num);
+      } 
+      _this.sum();
+    });
+    //输入数量
+    $('input[type=number]').change(function(){
+      var num = parseInt($(this).val()),
+          itemPrice = parseFloat($(this).parent('.bd-number').parent('.good-item').find('.itemPrice').find('b').html());
+      if(num >= 1){
+        $(this).val(num);
+      }else{
+        $(this).val(1);
+      }
+      _this.sum();
+    }); 
+    //删除单个
+    $('.delBtn').on('click',function(){
+      $(this).parent('.bd-operate').parent('.good-item').remove();
+      var selectedLen = $('.good-item').find('.bd-check').find('input:checked').length;
+      if(selectedLen === 0){
+        _this.nogoods();   
+      }
+      _this.sum();      
+    });
+    //删除所有
+    $('.delAllBtn').on('click',function(){
+      var allCheck = $(this).parent('.ft-del').prev('.ft-check').find('input[type=checkbox]').is(':checked');
+      if(allCheck){
+        $('.good-list').empty();
+      }
+      _this.nogoods(); 
+    }); 
+  },
+  //计算总数和总金额
+  sum: function(){
+    var _this = this,
+        sum = 0,
         itemSum = 0,
         num = 0,
         sumNum = 0;
@@ -32,30 +93,11 @@ $(function(){
       }    
       $('.ft-totalPrice').find('b').html(sum.toFixed(2));
     });
-  };
-
-  //全选
-  function checkAll(){
-    $('.checkAll').on('click',function(){
-      $('.data-table').find('input:checkbox').prop('checked',  $(this).prop('checked'));
-      isCheckAll();
-      sum();
-    });
-  };
-
-  //单个商品勾选
-  function checkItem(){
-    $('.checkboxItem').on('click',function(){
-      isCheckAll();
-      sum();
-    });
-  };
-
+  },
   //判断是否全选
-  function isCheckAll(){
+  isCheckAll: function() {    
     var len = $('.good-item').length,
         selectedLen = $('.good-item').find('.bd-check').find('input:checked').length;
-        // console.log('len: ' + len + ', selectedLen: '+selectedLen);
     if(len === selectedLen){
       $('.data-table').find('.checkAll').prop('checked', true);
     } else {
@@ -67,86 +109,17 @@ $(function(){
       $('.ft-totalPrice a').attr('class', 'disable');
       $('.ft-selectedGoods').find('b').html(0);
     }
-  };
-
-  //加
-  function plusFn(){
-    $('.plus').on('click',function(){
-      var num = parseInt($(this).prev('.number').val());
-      var itemPrice = parseFloat($(this).parent('.bd-number').parent('.good-item').find('.itemPrice').find('b').html());
-      num++;
-      $(this).prev('.number').val(num++);
-      sum();
-    }); 
-  };
-
-  //减
-  function minusFn(){
-    $('.minus').on('click',function(){
-      var num = parseInt($(this).next('.number').val());
-      var itemPrice = parseFloat($(this).parent('.bd-number').parent('.good-item').find('.itemPrice').find('b').html());
-      if(num > 1){
-        num--;
-        $(this).next('.number').val(num);
-      } else if(num===1) {
-        $(this).next('.number').val(1);
-        tipsBox('亲，数量不能再少啦');
-      } else {
-        $(this).next('.number').val(num);
-      } 
-      sum();
-    });
-  };
-
-  //输入数量
-  function inputFn(){
-    $('input[type=number]').change(function(){
-      var num = parseInt($(this).val()),
-          itemPrice = parseFloat($(this).parent('.bd-number').parent('.good-item').find('.itemPrice').find('b').html());
-      if(num >= 1){
-        $(this).val(num);
-      }else{
-        $(this).val(1);
-      }
-      sum();
-    });
-  };
-
-  //删除单个
-  function delFn(){
-    $('.delBtn').on('click',function(){
-      $(this).parent('.bd-operate').parent('.good-item').remove();
-      var selectedLen = $('.good-item').find('.bd-check').find('input:checked').length;
-      if(selectedLen === 0){
-        nogoods();   
-      }
-      sum();      
-    });
-  };
-
-  //批量删除
-  function delAll(){
-    $('.delAllBtn').on('click',function(){
-      var allCheck = $(this).parent('.ft-del').prev('.ft-check').find('input[type=checkbox]').is(':checked');
-      if(allCheck){
-        $('.good-list').empty();
-      }
-      nogoods(); 
-    });
-  };
-
-  //初始化
-  function nogoods(){
+  }, 
+  nogoods: function() {
     $('.ft-selectedGoods').find('b').html(0);
     $('.ft-totalPrice').find('b').html('0.00'); 
     $('.checkAll').prop('checked', false); 
     $('.ft-totalPrice a').attr('class', 'disable');
-  };
-
-  //提示
-  function tipsBox(str){
+  },
+  tipsBox: function(str) {
     $('.tipsBox').show().html(str);
     setTimeout(function(){$('.tipsBox').hide();},2000);
     return false;
-  };
-});
+  }
+}
+cartObj.init();
